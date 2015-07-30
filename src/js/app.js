d76bcb208c5d0ece8f5fe30f22bb906c9c4ab871
PhoneChan <WeiGe@liuliuchengweideMacBook-Pro.local>
2015-07-30 19:01:54 +0800

    error
    这是个错误的版本

--- src/js/app.js
+++ src/js/app.js
@@ -44,7 +44,7 @@ myApp.config(function($stateProvider, $urlRouterProvider) {
             url: '/order/:venueId',
             templateUrl: 'tpls/order.html'
         }).state('map', {
-            url: '/venuedetail/map',
+            url: '/venuedetail/map/:lat',
             templateUrl: 'tpls/map.html'
         });
 
--- src/js/directive.js
+++ src/js/directive.js
@@ -30,13 +30,13 @@ myApp.directive("baidumap", function () {
       latitude: "="
     },
     link: function (scope, element, attrs) {
-        var map;
-
         // 百度地图API功能
         var map = new BMap.Map("allmap");            // 创建Map实例
         var point = new BMap.Point(116.404, 39.915); // 创建点坐标
         map.centerAndZoom(point,15);                 // 初始化地图,设置中心点坐标和地图级别。
         map.addControl(new BMap.ZoomControl());      //添加地图缩放控件
+        marker = new BMap.Marker(point);  //创建标注
+        map.addOverlay(marker);                 // 将标注添加到地图中
 
 
       // //百度地图API功能
